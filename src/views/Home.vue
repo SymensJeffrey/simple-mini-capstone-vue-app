ee83a90d866af87b261c24efa81165c92c808317
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-19 19:20:43 -0500

    add update button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,6 @@
     <div v-for="product in products" v-bind:key="product.id">
       <p>{{ product.id }}</p>
       <p>{{ product.name }}</p>
-      <p>{{ product.price }}</p>
-      <p>{{ product.description }}</p>
       <p>
         <img
           :src="product.image_url"
@@ -21,15 +19,35 @@
           title="Product Image"
         />
       </p>
+      <p>{{ product.price }}</p>
+      <button v-on:click="showProduct(product)">Update Product</button>
       <hr />
     </div>
+    <dialog id="product-details">
+      <form method="dialog">
+        <p>hello</p>
+        <p>Name: <input type="text" v-model="currentProduct.name" /></p>
+        <p>
+          Description:
+          <input type="text" v-model="currentProduct.description" />
+        </p>
+        <p>
+          Image_url: <input type="text" v-model="currentProduct.image_url" />
+        </p>
+        <p>Price: <input type="text" v-model="currentProduct.price" /></p>
+        <button v-on:click="updateProduct(currentProduct)">Update</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
+
 <style>
 img {
   width: 250px;
 }
 </style>
+
 <script>
 import axios from "axios";
 export default {
@@ -38,6 +56,7 @@ export default {
       message: "Welcome to Vue.js!",
       products: [],
       newProductParams: {},
+      currentProduct: {},
     };
   },
   created: function () {
@@ -65,6 +84,22 @@ export default {
           console.log(response.data);
           this.products.push(response.data);
           this.newProductParams = {};
+        });
+    },
+    showProduct: function (theProduct) {
+      this.currentProduct = theProduct;
+      console.log(theProduct);
+      console.log("showing product");
+      document.querySelector("#product-details").showModal();
+    },
+    updateProduct: function (theProduct) {
+      console.log("updating....");
+      console.log(theProduct);
+      var editParams = theProduct;
+      axios
+        .patch("http://localhost:3000/products/" + editParams.id, editParams)
+        .then((response) => {
+          console.log(response.data);
         });
     },
   },
